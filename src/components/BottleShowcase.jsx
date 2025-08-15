d0fa6a8721464acecfaac5d052187f53dcd8f976
Ryan Connors <ryannumber3@gmail.com>
2025-08-14 23:59:16 -0400

    images done

--- src/components/BottleShowcase.jsx
+++ src/components/BottleShowcase.jsx
@@ -4,17 +4,17 @@ export default function BottleShowcase() {
   return (
     <section className="mt-20 px-6 w-full max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-20 items-center">
       <img
-        src="/Forest.jpg"
-        alt="Nantucket Edition"
-        className="w-auto max-h-[48rem] mx-auto object-contain"
-      />
-      <img
-        src="/citrus.jpg"
+        src="/Sorrentobottle.png"
         alt="Sorrento Edition"
         className="w-auto max-h-[48rem] mx-auto object-contain"
       />
       <img
-        src="/Ocean.jpg"
+        src="/Nantucketbottle.png"
+        alt="Nantucket Edition"
+        className="w-auto max-h-[48rem] mx-auto object-contain"
+      />
+      <img
+        src="/Balibottle.png"
         alt="Bali Edition"
         className="w-auto max-h-[48rem] mx-auto object-contain"
       />
